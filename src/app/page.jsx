c16905c78ed60a95f5b5fa7d0e6f9b87fedda473
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-06-05 11:25:38 +0500

    First Commit

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <Box fontSize={50} className={styles.main}>
+      Hy
+    </Box>
+  );
+}
