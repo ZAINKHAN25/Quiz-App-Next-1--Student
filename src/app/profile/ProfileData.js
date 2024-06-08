9e5885727a19d79286676206b8745839e9fad7b9
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-06-08 19:53:34 +0500

    Update in Profile Data

---
+++ src/app/profile/ProfileData.js
@@ -0,0 +1,24 @@
+const ProfileData = [
+    {
+        quizdate: "Mon Jan 30 2023",
+        courseName: "Web and mobile hybrid app develpment",
+        batch: 9,
+        quizsubject: "Css3",
+        quizName: "Css3 quiz 1",
+        quizScore: 68
+    },
+    {
+        quizdate: "Mon May 15 2023",
+        courseName: "Web and mobile hybrid app develpment",
+        batch: 9,
+        quizsubject: "Javascript",
+        quizName: "Javascript quiz 2",
+        quizScore: 64
+    }
+];
+
+const ProfileHeadingArr = [
+    "#", "Date", "Course", "Batch", "Subject", "Quiz", "Score"
+]
+
+export {ProfileData, ProfileHeadingArr};
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,3 +1,83 @@
+import { Box, Heading, Table, TableContainer, Tbody, Th, Thead, Tr } from "@chakra-ui/react";
+
+import Navbar from "../components/navbar/index.jsx";
+import colorThemes from "../Color.js";
+import { ProfileData, ProfileHeadingArr } from './ProfileData.js';
+
 export default function Page() {
-  return <h1>Hello, Profile page!</h1>
+  return (
+    <Box
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
+    >
+      <Navbar />
+      <Box
+        width={"85
+        
+        
+        vw"}
+        pt={8}
+        pb={5}
+        borderBottom={"1px solid rgba( 0, 0, 0, 10%)"}
+        mb={3}
+      >
+        <Heading
+          color={colorThemes.lightColor.secondColor}
+          fontWeight={300}
+          fontSize={45}
+        >
+          Results
+        </Heading>
+
+        {/* Table Content */}
+        <TableContainer
+          my={15}
+          border={"transparent"}
+        >
+          <Table variant='simple' border={"transparent"}>
+            <Thead>
+              <Tr>
+                {
+                  ProfileHeadingArr.map((x, i) => {
+                    return (
+                      <Th
+                        key={i}
+                        color={colorThemes.lightColor.secondColor}
+                        fontSize={15}
+                      >
+                        {x}
+                      </Th>
+                    )
+                  })
+                }
+              </Tr>
+            </Thead>
+            <Tbody>
+
+              {
+                ProfileData.map((x, i) => {
+                  return (
+                    <Tr
+                      key={i}
+                    >
+                      <Th fontSize={14} fontWeight={400}>{i + 1}</Th>
+                      <Th fontSize={14} fontWeight={400}>{x.quizdate}</Th>
+                      <Th fontSize={14} fontWeight={400}>{x.courseName}</Th>
+                      <Th fontSize={14} fontWeight={400}>{x.batch}</Th>
+                      <Th fontSize={14} fontWeight={400}>{x.quizsubject}</Th>
+                      <Th fontSize={14} fontWeight={400}>{x.quizName}</Th>
+                      <Th fontSize={14} fontWeight={400}>{x.quizScore}</Th>
+                    </Tr>
+                  )
+                })
+              }
+            </Tbody>
+          </Table>
+        </TableContainer>
+
+      </Box>
+    </Box>
+  )
 }
