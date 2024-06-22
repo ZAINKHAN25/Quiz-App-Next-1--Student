5dfbb7a88fa219ab1e81c05ef29e898d0cff4a75
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-06-22 17:26:49 +0500

    Some Changes

--- src/app/quiz/page.jsx
+++ src/app/quiz/page.jsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 
 import { Box, Text } from "@chakra-ui/react";
 import dummyData from "../DummyData.js";
@@ -6,26 +6,23 @@ import { useState, useEffect } from "react";
 
 export default function Page() {
   const initialQuizTime = dummyData[0].classesOfQuiz[0].subjects[0].quizTime;
-  const [quizTime, setQuizTime] = useState(initialQuizTime);
   const [quizSeconds, setQuizSeconds] = useState(initialQuizTime * 60);
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (quizSeconds > 0) {
-        setQuizSeconds(prevSeconds => prevSeconds - 1);
+        setQuizSeconds((prevSeconds) => prevSeconds - 1);
       } else {
         clearInterval(interval);
       }
     }, 1000);
-
     return () => clearInterval(interval);
   }, []);
 
   const minutes = Math.floor(quizSeconds / 60);
   const seconds = quizSeconds % 60;
 
-  
-  const formattedTime = `${minutes.toString().padStart(2, '0')} : ${seconds.toString().padStart(2, '0')}`;
+  const formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 
   return (
     <Box>
